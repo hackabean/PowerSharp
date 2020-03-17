4260c936c9e137c7600a62409f8f8beb73a0a76e
hackabean <50614334+hackabean@users.noreply.github.com>
2020-03-17 16:20:06 +0000

    Create EncodeAndCompress.sh

---
+++ EncodeAndCompress.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Provide full path to powershell script to encode and compress"
+file=$1
+gzip $file -c | base64 -w 0 > /tmp/gzippedB64
+echo "\$Decoded = [System.Convert]::FromBase64String(\"" > /tmp/build
+cat /tmp/gzippedB64 >> /tmp/build 
+echo "\");\$ms = (New-Object System.IO.MemoryStream(\$Decoded,0,\$Decoded.Length)); iex(New-Object System.IO.StreamReader(New-Object System.IO.Compression.GZipStream(\$ms, [System.IO.Compression.CompressionMode]::Decompress))).readtoend()" >> /tmp/build
+cat /tmp/build | base64 -w 0 > /tmp/EncodedPowerShellScript
+echo "Done, enocded and compressed script available in /tmp/EncodedPowerShellScript"
